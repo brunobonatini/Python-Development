cfc75341fc587b816d5ac0c9b735f2a3e46af6fd
Bruno Spagnol Bonatini <brunospagnol@gmail.com>
2023-09-22 13:15:26 -0300

    Add exercise

--- CursoEmVideo/modulo2/ex055peso.py
+++ CursoEmVideo/modulo2/ex055peso.py
@@ -1,2 +1,18 @@
 # Faça um programam que leia o peso de cinco pessoas.
 # No final, mostre qual foi o maior e o menor peso lidos.
+
+PESO_MAIOR = 0
+PESO_MENOR = 0
+
+for i in range(1, 6):
+    peso = float(input(f'Digite o peso da {i}ª pessoa: '))  
+    if i == 1:
+        peso_maior = peso
+        peso_menor = peso
+    else:
+        if peso > peso_maior:
+            peso_maior = peso
+        if peso < peso_menor:
+            peso_menor = peso
+print(f'Peso maior: {peso_maior}')
+print(f'Peso menor: {peso_menor}')
--- CursoEmVideo/modulo2/ex056nomeidadesexo.py
+++ CursoEmVideo/modulo2/ex056nomeidadesexo.py
@@ -1,5 +1,30 @@
 # Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas.
-# Mostre: 
+# Mostre:
 # A média de idade do grupo;
 # Qual o nome do homem mais velho;
 # Quantas mulheres tem menos de 20 anos.
+
+SOMA_IDADE = 0
+MEDIA_IDADE = 0
+MULHERES = 0
+NOME_HOMEM_VELHO = ''
+IDADE_HOMEM_VELHO = 0
+
+for p in range(1, 5):
+    nome = input(f'Digite o nome da {p}ª pessoa: ').strip()
+    idade = int(input(f'Digite a idade da {p}ª pessoa: '))
+    sexo = input(f'Digite o sexo da {p}ª pessoa (M/F): ').strip()
+    SOMA_IDADE += idade
+    if p == 1 and sexo in 'Mn':
+        NOME_HOMEM_VELHO = nome
+        IDADE_HOMEM_VELHO = idade
+    if sexo in 'Mn' and idade > IDADE_HOMEM_VELHO:
+        IDADE_HOMEM_VELHO = idade
+        NOME_HOMEM_VELHO = nome
+    if sexo in 'Ff' and idade < 20:
+        MULHERES += 1
+media_idade = SOMA_IDADE / 4
+print(f'Média de idade do grupo: {media_idade} anos')
+print(f'Homem mais velho: {NOME_HOMEM_VELHO}')
+print(f'Idade do {NOME_HOMEM_VELHO}: {IDADE_HOMEM_VELHO} anos')
+print(f'Quantidade de mulheres com menos de 20 anos: {MULHERES}')
