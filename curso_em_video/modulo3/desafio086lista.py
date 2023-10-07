6de00fda1d3f77950134a846c8d0b4eb4ed8c420
Bruno Spagnol Bonatini <brunospagnol@gmail.com>
2023-10-07 12:27:16 -0300

    Update desafio086lista.py

--- curso_em_video/modulo3/desafio086lista.py
+++ curso_em_video/modulo3/desafio086lista.py
@@ -2,7 +2,7 @@
 # No final, mostre a matriz na tela, com a formatação correta.
 
 matriz = [[], [], []]
-
+'''
 for i in range(1, 4):
     valores1 = int(input('Digite um valor: '))
     matriz[0].append(valores1)
@@ -15,4 +15,7 @@ for i in range(1, 4):
     valores3 = int(input('Digite um valor: '))
     matriz[2].append(valores3)
 
-print(matriz) 
+print(matriz)
+'''
+for linha in range(0, 3):
+    for coluna in range(0, 3)
