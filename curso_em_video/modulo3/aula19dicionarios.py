a3600fe8d4466f17ec0dbd399fe4bdc8d3921ccd
Bruno Spagnol Bonatini <brunospagnol@gmail.com>
2023-10-09 22:57:52 -0300

    Add exercise

---
+++ curso_em_video/modulo3/aula19dicionarios.py
@@ -0,0 +1,14 @@
+# criando um dicionário
+pessoas = {'nome': 'Bruno', 'sexo': 'M', 'idade': 30}
+print(pessoas)
+print(pessoas['nome'])
+print(f'{pessoas["nome"]} tem {pessoas["idade"]} anos')
+
+# mostrando as chaves na tela
+print(pessoas.keys())
+
+# mostrando os valores na tela
+print(pessoas.values())
+
+# mostrando os itens na tela
+print(pessoas.items())
