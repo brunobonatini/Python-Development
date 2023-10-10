81474c7bf414b9198941ed5c68e0ea498968115e
Bruno Spagnol Bonatini <brunospagnol@gmail.com>
2023-10-10 11:56:31 -0300

    Add exercise

---
+++ curso_intensivo_python/cap03listas.py
@@ -0,0 +1,19 @@
+bicycles = ['caloi', 'monark', 'ceci', 'jna']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[-1])
+print(bicycles[1].title())
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
+
+amigos = ['Leticia', 'Ozzy', 'Paulo']
+print(amigos[0])
+print(amigos[1])
+print(amigos[2])
+
+mensagem_amigos = 'Hello my friend ' + amigos[0] + '.'
+print(mensagem_amigos)
+
+carros = ['honda', 'toyota', 'hyundai']
+print(carros)
